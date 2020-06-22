6dc389c497db11cca012211a385b67b7b005698d
jackowensii@gmail.com <jackowens@Jacks-MacBook-Pro.local>
2020-06-22 11:49:44 -0400

    longest word calculator

--- script.js
+++ script.js
@@ -1,31 +1,28 @@
-function longestWordLength(text) {
-  let wordArray = text.split(' ')
-  let maxLength = 0
-  let result = ''
-     console.log(wordArray);
-  for (let i = 0; i < wordArray.length; i++) {
-  console.log('length');
-   console.log(wordArray[i]);
-  
-    if (wordArray[i].length > maxLength) {
-      maxLength = wordArray[i].length;
-      result = wordArray[i]
-    }
-  }
-  return result.length;
-}
-
 function longestWord(text) {
   let wordArray = text.split(' ')
   let maxLength = 0
   let result = ''
+
   for (let i = 0; i < wordArray.length; i++) {
     if (wordArray[i].length > maxLength) {
       maxLength = wordArray[i].length
       result = wordArray[i]
     }
   }
-  return result;
+
+  return result
+}
+
+
+function longestWordLength(str) {
+   var words = str.split(' ');
+   var longest = 0;
+   for (var i=0;i<words.length;i++) {
+        if (words[i].length > longest) {
+             longest = words[i].length;
+        }  
+   }
+   return longest;
 }
 
 
@@ -33,20 +30,23 @@ function longestWord(text) {
 $(function() {
   module("Longest Word");
   test("longestWord()", t => {
-    t.equal(longestWord('wind and willows'), "willows", "longest word testert");
+    t.equal(longestWord('wind and willows'), "willows", "wind and willow test");
+  });
+  
+    test("longestWord()", t => {
+    t.equal(longestWord('cats in the cradle'), "cradle", "cats in the cradle test");
   });
 
-    test("longestWordLength()", t => {
-    t.equal(longestWordLength('example two'), 7 , " length test");
+  test("longestWordLength()", t => {
+    t.equal(longestWordLength('example two'), 7, " length test");
   });
   
-      test("longestWordLength()", t => {
-    t.equal(longestWordLength('Paul Art Simon'), 5 , " length test");
+   test("longestWordLength()", t => {
+    t.equal(longestWordLength('Paul Simon'), 5, " length test");
   });
-  
+
 
   QUnit.done = function(stats) {
     console.log('QUnit Done: ' + stats.total + ' tests run, ' + stats.passed + ' tests passed, ' + stats.failed + ' tests failed');
   };
-}); // ready for the test
-
+}); // ready
