771a2990c4f76049d8bc9c95abe75098bd171e96
jackowensii@gmail.com <jackowens@Jacks-MacBook-Pro.local>
2020-06-21 23:03:59 -0400

    longest word calculator

---
+++ script.js
@@ -0,0 +1,52 @@
+function longestWordLength(text) {
+  let wordArray = text.split(' ')
+  let maxLength = 0
+  let result = ''
+     console.log(wordArray);
+  for (let i = 0; i < wordArray.length; i++) {
+  console.log('length');
+   console.log(wordArray[i]);
+  
+    if (wordArray[i].length > maxLength) {
+      maxLength = wordArray[i].length;
+      result = wordArray[i]
+    }
+  }
+  return result.length;
+}
+
+function longestWord(text) {
+  let wordArray = text.split(' ')
+  let maxLength = 0
+  let result = ''
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray[i].length > maxLength) {
+      maxLength = wordArray[i].length
+      result = wordArray[i]
+    }
+  }
+  return result;
+}
+
+
+
+$(function() {
+  module("Longest Word");
+  test("longestWord()", t => {
+    t.equal(longestWord('wind and willows'), "willows", "longest word test");
+  });
+
+    test("longestWordLength()", t => {
+    t.equal(longestWordLength('example two'), 7 , " length test");
+  });
+  
+      test("longestWordLength()", t => {
+    t.equal(longestWordLength('Paul Simon'), 5 , " length test");
+  });
+  
+
+  QUnit.done = function(stats) {
+    console.log('QUnit Done: ' + stats.total + ' tests run, ' + stats.passed + ' tests passed, ' + stats.failed + ' tests failed');
+  };
+}); // ready
+
